0edd6d51aa74fee89a6940fb34916123c0a0c749
Hippopcorn <elsa.varache@gmail.com>
2025-01-11 16:17:41 +0100

    pointeurs

--- header/main.c
+++ header/main.c
@@ -8,6 +8,11 @@ int main(int argc, char *argv[])
     double resultat;
     resultat = aireRectangle(10.0, 25.0);
     printf("L'aire du rectangle est de %f\n", resultat);
+    printf("L'adresse de la variable resultat est : %p\n", &resultat);
+
+    double *pointeurSurResultat = &resultat;
+    printf("Le pointeur contient l'adresse %p\n", pointeurSurResultat);
+    printf("La valeur contenue à l'adresse du pointeur est : %f\n", *pointeurSurResultat);
 
     return 0;
 }
